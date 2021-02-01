45504f2ca0a8a9d22c8f434fd07daa633198df6d
InMyConnection <nadin114@yandex.ru>
2021-02-02 02:16:20 +0300

    Walls

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,33 +8,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
+            Console.SetBufferSize(80, 25);
 
-            Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
-
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach (var f in figures)
-                f.Draw();
-            
-            /*Console.SetBufferSize(80, 25);
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
@@ -46,6 +23,8 @@ namespace Snake
 
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                    break;
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
@@ -63,11 +42,7 @@ namespace Snake
                 }
                 Thread.Sleep(100);
                 snake.Move();
-            }*/
-        }
-        static void Draw (Figure figure)
-        {
-            figure.Draw();
+            }
         }
     }
 }
