fd527667cbe02037b0616e505ec007fd1dff7e49
InMyConnection <nadin114@yandex.ru>
2021-02-15 22:36:29 +0300

    tanks

---
+++ Tanks/Tanks/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Tanks
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtScore = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(505, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(176, 82);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Score: 0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(580, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Form1";
+            this.Text = "Battle City";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tanks/Tanks/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tanks
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft = false;
+        bool goRight = false;
+        bool goUp = false;
+        bool goDown = false;
+        bool shooting = false;
+
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateBitmapAtRuntime();
+        }
+
+        PictureBox pictureBox1 = new PictureBox();
+        public void CreateBitmapAtRuntime()
+        {
+            pictureBox1.Size = new Size(500, 360);
+            this.Controls.Add(pictureBox1);
+
+            //Bitmap map = new Bitmap(490, 360);
+
+            Bitmap image = new Bitmap("Up.png");
+            pictureBox1.Image = image;
+            pictureBox1.Invalidate();
+
+            //Graphics mapGraphics = Graphics.FromImage(map);
+
+            //mapGraphics.FillRectangle(Brushes.Orange, 20, 20, 450, 20);
+            //mapGraphics.FillRectangle(Brushes.Blue, 100, 100, 50, 50);
+            //mapGraphics.FillEllipse(Brushes.Yellow, 200, 200, 50, 50);
+
+            //pictureBox1.Image = map;
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                shooting = true;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                shooting = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
